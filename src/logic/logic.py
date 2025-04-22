b8ff41fc0ecb7ff2be08c56f683f9a0a7bebe790
ilya-groza <ilya.groza13@gmail.com>
2025-04-22 18:47:52 +0400

    feat: rewrite logic of work with using models

--- src/logic/logic.py
+++ src/logic/logic.py
@@ -2,7 +2,8 @@ import time
 
 from config import VK_TOKEN, VK_GROUP
 from src.api_handlers.vk_api_handler import VKAPIHandler
-from src.post_processing import PostProcessor, PostCategorizer, TableCreator
+from src.post_processing.post_analyzer import PostAnalyzer
+from src.report.report_builder import ReportBuilder
 
 
 class MainLogic:
@@ -10,67 +11,31 @@ class MainLogic:
         pass
 
     # Основной скрипт
-    def run(self, fio, start_date, end_date, update_progress=None, group_url=VK_GROUP):  # Читаем токен из файла
+    def run(self, fio, start_date, end_date, update_progress=None, group_name=VK_GROUP):  # Читаем токен из файла
 
         try:
             # Инициализация классов
             vk_handler = VKAPIHandler(VK_TOKEN)
-            post_processor = PostProcessor(fio)
-            post_categorizer = PostCategorizer()
-            table_creator = TableCreator()
+            post_analyzer = PostAnalyzer(fio)
+            report_builder = ReportBuilder()
 
-            start_time = time.time()
             # Получаем ID группы
-            group_id = vk_handler.get_group_id(group_url)
-            print("Время на Получаем ID группы: ", time.time() - start_time)
-            start_time = time.time()
+            group_id = vk_handler.get_group_id(group_name)
 
             # Получаем посты
+            start_time = time.time()
             posts = vk_handler.get_posts(group_id, start_date, end_date, update_progress)
-            print("Время на Получаем посты: ", time.time() - start_time)
+            print("Время на Получение постов: ", time.time() - start_time)
+
+            # Анализируем посты
             start_time = time.time()
+            filtered_posts = post_analyzer.posts_analyze(posts)
+            print("Время на Обработку постов: ", time.time() - start_time)
 
-            # Фильтруем посты
-            filtered_posts = post_processor.filter_posts(posts)
-            print("Время на Фильтруем посты: ", time.time() - start_time)
-            start_time = time.time()
+            for post in filtered_posts:
+                print(post.__str__())
 
-            # Группируем посты
-            categorized_posts = post_categorizer.categorize_posts(filtered_posts, fio)
-            print("Время на Группируем посты: ", time.time() - start_time)
-            start_time = time.time()
-
-            # Открываем файлы для записи
-            with open("posts.txt", "w", encoding="utf-8") as posts_file:
-                with open("for_word_file.txt", "w", encoding="utf-8") as for_word_file:
-                    # Выводим результаты в консоль и записываем в файл
-                    for category, posts in categorized_posts.items():
-                        # Запись в файл
-                        for_word_file.write(f"\n{category}. Постов в категории: {len(posts)}\n")
-                        table = table_creator.create_table(posts, group_id, post_processor)
-                        for_word_file.write(table + "\n")  # Записываем таблицу в файл
-
-                        # Вывод в консоль
-                        posts_file.write(f"\n{category}. Постов в категории: {len(posts)}\n\n")
-                        for post in posts:
-                            title = post_processor.extract_title(post['text'])
-                            post_date = post_processor.format_date(post['date'])
-                            chars_before_pattern = post_processor.count_chars_before_pattern(post['text'])
-                            post_link = post_processor.get_post_link(group_id, post['id'])
-
-                            posts_file.write(f"Название: {title}\n"
-                                             f"Дата: {post_date}\n"
-                                             f"Количество символов: {chars_before_pattern}\n"
-                                             f"Ссылка на пост: {post_link}\n"
-                                             f"{'-' * 40}\n")
-
-                    # Итоговое количество постов
-                    total_posts = f"\nВсего подходящих постов: {len(filtered_posts)}"
-                    print(total_posts)
-                    posts_file.write(total_posts + "\n")
-
-                print("\nРезультаты успешно записаны в файл for_word_file.txt")
-                print("\nРезультаты успешно записаны в файл posts.txt")
+            report_builder.report_builder(filtered_posts)
 
         except Exception as e:
             print(f"Произошла ошибка: {e}")
