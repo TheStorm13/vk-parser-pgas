30ebceb49978f4e1b7a8e23652eeccf361659a1c
ilya-groza <ilya.groza13@gmail.com>
2025-04-26 12:41:43 +0400

    feat: optimize post retrieval and add logging
    improve logic in `get_posts` method for better filtering and result building; integrate logging with `setup_logger`

--- src/logic/api_handlers/vk_api_handler.py
+++ src/logic/api_handlers/vk_api_handler.py
@@ -1,6 +1,8 @@
-from datetime import datetime
-
 import vk_api
+
+from src.logger.logger import setup_logger
+
+logger = setup_logger(__name__)
 
 
 class VKAPIHandler:
@@ -14,8 +16,10 @@ class VKAPIHandler:
         Получает ID группы по её короткому имени.
         """
         group_info = self.vk.utils.resolveScreenName(screen_name=group_url)
+
         if not group_info or group_info['type'] != 'group':
             raise ValueError("Группа не найдена!")
+
         return -group_info['object_id']
 
     def get_posts(self, owner_id, start_date, end_date, update_progress=None):
@@ -23,36 +27,54 @@ class VKAPIHandler:
         """
         Получает посты из группы за указанный период.
         """
-        posts = []
-        offset = 0
-        count = 100  # Максимальное количество постов за один запрос
+        result_posts = []
         total_posts = 0
         progress = 0
 
+        offset = 0
+        count = 100  # Максимальное количество постов за один запрос
+
+        get_time = 0
+        process_time = 0
+
         while True:
             try:
-                response = self.vk.wall.get(owner_id=owner_id, count=count, offset=offset)
-                if not response['items']:
+                response = self.vk.wall.get(owner_id=owner_id,
+                                            count=count,
+                                            offset=offset)
+                posts = response['items']
+
+                if not posts:
                     break
 
-                total_posts += len(response['items'])
-
-                for post in response['items']:
-                    progress += 1
-                    if update_progress:
-                        update_progress(progress, total_posts)
-                    if post['comments']['count'] > 0:
-                        post['comments'] = self.vk.wall.getComments(owner_id=owner_id, post_id=post['id'])
-                    post_date = datetime.fromtimestamp(post['date'])
-                    if start_date <= post_date <= end_date:
-                        posts.append(post)
-                    elif post_date < start_date:
-                        return posts  # Прекращаем, если посты старше начала периода
-
+                total_posts += len(posts)
                 offset += count
 
+                first_date = posts[0]['date']
+                last_date = posts[-1]['date']
+
+                if last_date > end_date:
+                    continue
+
+                if first_date < start_date:
+                    return result_posts
+
+                for post in posts:
+                    progress += 1
+                    post_date = (post['date'])
+
+                    if update_progress:
+                        update_progress(progress, total_posts)
+
+                    if start_date <= post_date <= end_date:
+                        if post['comments']['count'] > 0:
+                            post['comments'] = self.vk.wall.getComments(owner_id=owner_id,
+                                                                        post_id=post['id'])
+                        result_posts.append(post)
+
+
             except vk_api.exceptions.ApiError as e:
-                print(f"Ошибка API: {e}")
+                logger.error(f"Ошибка API: {e}")
                 break
 
-        return posts
+        return result_posts
