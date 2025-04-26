48c8f643d2e546932fb68145c52db255785429ed
ilya-groza <ilya.groza13@gmail.com>
2025-04-26 14:36:18 +0400

    perf: optimize post retrieval and add logging

--- src/logic/api_handlers/vk_api_handler.py
+++ src/logic/api_handlers/vk_api_handler.py
@@ -10,32 +10,21 @@ class VKAPIHandler:
         self.vk_session = vk_api.VkApi(token=token)
         self.vk = self.vk_session.get_api()
 
-    def get_group_id(self, group_url):
-        # todo: добавить логирование и типизацию
-        """
-        Получает ID группы по её короткому имени.
-        """
-        group_info = self.vk.utils.resolveScreenName(screen_name=group_url)
+    def get_group_id(self, group_name):
+        # Resolves VK group ID from its name
+        group_info = self.vk.utils.resolveScreenName(screen_name=group_name)
 
         if not group_info or group_info['type'] != 'group':
             raise ValueError("Группа не найдена!")
 
         return -group_info['object_id']
 
-    def get_posts(self, owner_id, start_date, end_date, update_progress=None):
-        # todo: добавить логирование и типизацию
-        """
-        Получает посты из группы за указанный период.
-        """
+    def handler_posts(self, owner_id, start_date, end_date):
         result_posts = []
         total_posts = 0
-        progress = 0
 
         offset = 0
-        count = 100  # Максимальное количество постов за один запрос
-
-        get_time = 0
-        process_time = 0
+        count = 100  # Maximum number of posts per request
 
         while True:
             try:
@@ -57,24 +46,41 @@ class VKAPIHandler:
                     continue
 
                 if first_date < start_date:
-                    return result_posts
+                    break
 
-                for post in posts:
-                    progress += 1
-                    post_date = (post['date'])
-
-                    if update_progress:
-                        update_progress(progress, total_posts)
-
-                    if start_date <= post_date <= end_date:
-                        if post['comments']['count'] > 0:
-                            post['comments'] = self.vk.wall.getComments(owner_id=owner_id,
-                                                                        post_id=post['id'])
-                        result_posts.append(post)
-
+                result_posts.extend(posts)
 
             except vk_api.exceptions.ApiError as e:
                 logger.error(f"Ошибка API: {e}")
                 break
+
+        logger.info("Posts are fetched")
+
+        return result_posts
+
+    def get_posts(self, group_name, start_date, end_date, update_progress=None):
+        result_posts = []
+        progress = 0
+
+        owner_id = self.get_group_id(group_name)
+
+        posts = self.handler_posts(owner_id, start_date, end_date)
+
+        for post in posts:
+            progress += 1
+            post_date = (post['date'])
+
+            if update_progress:
+                update_progress(progress, len(posts))
+
+            # Filter posts within the date range
+            if start_date <= post_date <= end_date:
+                # Fetch comments if any are available
+                if post['comments']['count'] > 0:
+                    post['comments'] = self.vk.wall.getComments(owner_id=owner_id,
+                                                                post_id=post['id'])
+                result_posts.append(post)
+
+        logger.info("Posts filtered by date")
 
         return result_posts
