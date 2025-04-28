c1778c13b9c9d1f0265d68fa19e55722b831f4e0
ilya-groza <ilya.groza13@gmail.com>
2025-04-29 00:20:42 +0400

    feat: update author validation logic for improved error handling
    - rename fio to full_name for clarity
    - enhance split handling with error logging for invalid full_name input
    - refactor regex pattern creation to better manage direct/reversed order matching

--- src/core/post_processing/author_validator.py
+++ src/core/post_processing/author_validator.py
@@ -6,48 +6,51 @@ logger = setup_logger(__name__)
 
 
 class AuthorValidator:
-    def __init__(self, fio):
-        self.fio = fio
+    def __init__(self, full_name):
+        self.full_name = full_name
+        self.pattern = self.create_pattern(full_name)
 
     @staticmethod
-    def create_pattern(fio: str) -> re.Pattern:
-        surname, name, patronymic = fio.split()
+    def create_pattern(full_name: str) -> re.Pattern:
+        try:
+            surname, name = full_name.split()
+        except ValueError as e:
+            logger.error(f"ValueError: {e}")
+            raise ValueError("Неправильно введено Фамилия имя!")
 
-        prefix = r"(Автор|Текст)\s*:?\s*"
+        prefix = r"((Автор|Текст)\s*:?\s*)"
 
         # Handle full name and initial variations
-        name_variations = rf"({name[0]}\.?\s*|{name}\s*)"
-        patronymic_variations = rf"({patronymic[0]}\.?\s*|{patronymic}\s*)"
+        name_variations = rf"(({name[0]}\.?)|({name}))\s*"
 
         # Pattern for both direct and reversed order
-        direct_order = rf"{surname}\s+{name_variations}({patronymic_variations})?"
-        reversed_order = rf"{name_variations}({patronymic_variations})?{surname}"
+        direct_order = rf"({surname}\s*{name_variations})"
+        reversed_order = rf"({name_variations}{surname})"
 
         # Combine patterns
-        pattern = rf"^{prefix}({direct_order}|{reversed_order})\s*\.?\s*$"
+        pattern = rf"(\s*{prefix}({direct_order}|{reversed_order})\s*(\.|\s|$)\s*)"
 
         return re.compile(pattern, re.IGNORECASE)
 
     def check_author(self, text: str):
         # Check if the author name appears in the provided text
-        pattern = AuthorValidator.create_pattern(self.fio)
-        result = pattern.search(text)
+        result = self.pattern.search(text)
 
         return result
 
     def validate_author(self, post) -> bool:
         # Validate if the author is mentioned in the post or its comments
-        if self.check_author(post['text']):
+        if self.check_author(post["text"]):
             logger.debug("The author is indicated in the text of the post")
             return True
 
         # Check each comment for the author name
-        if post['comments']['count'] > 0:
-            comments = post['comments']['items']
+        if post["comments"]["count"] > 0:
+            comments = post["comments"]["items"]
             for comment in comments:
-                if self.check_author(comment['text']):
+                if self.check_author(comment["text"]):
                     logger.debug("The author is indicated in the comments of the post")
-                return True
+                    return True
 
         logger.debug("The author is not discovered at the post")
         return False
